c8fcefab809010862b900f0b7c81e021d225d1e0
Maksim Kiselev <kiselev.maksim1223@gmail.com>
2025-04-08 00:10:00 +0400

    add scss modules support; add prettier

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -20,12 +20,28 @@ module.exports = {
       },
       {
         test: /\.s[ac]ss$/i,
+        exclude: /\.module\.s[ac]ss$/i,
         use: [
           'style-loader',
           'css-loader',
           'sass-loader'
         ]
-      }
+      },
+      {
+        test: /\.module\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:5]', // Customize class name format
+              },
+            }
+          },
+          'sass-loader'
+        ]
+      },
     ]
   },
   plugins: [
